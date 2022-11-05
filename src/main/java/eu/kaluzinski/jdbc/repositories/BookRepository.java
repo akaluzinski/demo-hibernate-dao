f339083b3639b751b82257d0fd0235165846f409
Adrian <kaluza@gmail.com>
2022-11-05 14:00:00 +0100

    Extend BookDao

--- src/main/java/eu/kaluzinski/jdbc/repositories/BookRepository.java
+++ src/main/java/eu/kaluzinski/jdbc/repositories/BookRepository.java
@@ -4,11 +4,14 @@ import eu.kaluzinski.jdbc.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findBookByTitle(String title);
 
     Optional<Book> findBooksByIsbn(String isbn);
+
+    Stream<Book> findAllByTitleNotNull();
 
 }
