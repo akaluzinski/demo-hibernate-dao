093cecf0a77dc14a8a7bac93db0df2c6fb624005
Adrian <kaluza@gmail.com>
2022-11-07 18:45:40 +0100

    Add named param in repo

--- src/main/java/eu/kaluzinski/jdbc/repositories/AuthorRepository.java
+++ src/main/java/eu/kaluzinski/jdbc/repositories/AuthorRepository.java
@@ -4,6 +4,7 @@ package eu.kaluzinski.jdbc.repositories;
 import eu.kaluzinski.jdbc.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +12,8 @@ import java.util.Optional;
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 
-    @Query("SELECT a FROM Author a where a.lastName LIKE ?1%")
-    List<Author> findAllAuthorsByLastNameLikeQuery(String authorLastName);
+    @Query("SELECT a FROM Author a where a.lastName LIKE :lastName%")
+    List<Author> findAllAuthorsByLastNameLikeQuery(@Param("lastName") String lastName);
 
     Optional<Author> findAuthorByFirstNameAndLastName(String firstName, String lastName);
 
