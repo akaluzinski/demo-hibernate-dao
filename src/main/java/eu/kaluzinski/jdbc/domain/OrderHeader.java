887b9c162852916cb82f987bc3a5c8d17b71e9f6
Adrian <kaluza@gmail.com>
2022-11-16 18:19:13 +0100

    Add Address embeddable object

--- src/main/java/eu/kaluzinski/jdbc/domain/OrderHeader.java
+++ src/main/java/eu/kaluzinski/jdbc/domain/OrderHeader.java
@@ -1,5 +1,9 @@
 package eu.kaluzinski.jdbc.domain;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +13,22 @@ import java.util.Objects;
 @Setter
 @Getter
 @Entity
+@AttributeOverrides({
+        @AttributeOverride(name = "shippingAddress.address", column = @Column(name = "shipping_address")),
+        @AttributeOverride(name = "shippingAddress.city", column = @Column(name = "shipping_city")),
+        @AttributeOverride(name = "shippingAddress.state", column = @Column(name = "shipping_state")),
+        @AttributeOverride(name = "shippingAddress.zipCode", column = @Column(name = "shipping_zip_code")),
+        @AttributeOverride(name = "billToAddress.address", column = @Column(name = "bill_to_address")),
+        @AttributeOverride(name = "billToAddress.city", column = @Column(name = "bill_to_city")),
+        @AttributeOverride(name = "billToAddress.state", column = @Column(name = "bill_to_state")),
+        @AttributeOverride(name = "billToAddress.zipCode", column = @Column(name = "bill_to_zip_code"))
+})
 public class OrderHeader extends BaseEntity {
 
-
+    @Embedded
+    private Address shippingAddress;
+    @Embedded
+    private Address billToAddress;
     private String customer;
 
     @Override
@@ -22,12 +39,18 @@ public class OrderHeader extends BaseEntity {
 
         OrderHeader that = (OrderHeader) o;
 
+        if (!Objects.equals(shippingAddress, that.shippingAddress))
+            return false;
+        if (!Objects.equals(billToAddress, that.billToAddress))
+            return false;
         return Objects.equals(customer, that.customer);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
+        result = 31 * result + (shippingAddress != null ? shippingAddress.hashCode() : 0);
+        result = 31 * result + (billToAddress != null ? billToAddress.hashCode() : 0);
         result = 31 * result + (customer != null ? customer.hashCode() : 0);
         return result;
     }
