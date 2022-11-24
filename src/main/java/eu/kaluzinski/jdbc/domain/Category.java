63d9ffcea7ef9eec2eb99b490540c42a25d7a1dd
Adrian <kaluza@gmail.com>
2022-11-24 20:48:11 +0100

    Simplify equals/hashCode for category&product

--- src/main/java/eu/kaluzinski/jdbc/domain/Category.java
+++ src/main/java/eu/kaluzinski/jdbc/domain/Category.java
@@ -4,14 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-import java.util.Objects;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
 @Entity
 public class Category extends BaseEntity {
     private String description;
@@ -22,23 +19,4 @@ public class Category extends BaseEntity {
     )
     private Set<Product> products;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        Category category = (Category) o;
-
-        if (!Objects.equals(description, category.description))
-            return false;
-        return Objects.equals(products, category.products);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        return result;
-    }
 }
